64b659bd2848651679f8fd25b205364cfa049fcf
desalix <diegui8008@gmail.com>
2026-09-23 23:17:18 +0200

    Layout changes

---
+++ src/data/business.ts
@@ -0,0 +1,33 @@
+// Datos del taller. Cambia aquí y se actualiza en toda la web.
+
+export const name = 'Autotalleres Carmovil';
+export const foundedYear = 1991;
+
+export const address = {
+	street: 'C/ La Fragua 12',
+	city: 'Tres Cantos, Madrid',
+};
+export const mapsUrl = 'https://www.google.com/maps/search/?api=1&query=Autotalleres+Carmovil+C%2F+La+Fragua+12+Tres+Cantos';
+export const mapsEmbedUrl = 'https://www.google.com/maps?q=C%2F+La+Fragua+12,+Tres+Cantos,+Madrid&output=embed';
+
+export const phone = '91 803 43 53';
+export const phoneHref = 'tel:+34918034353';
+
+// Déjalo vacío ('') para no mostrarlo.
+export const email = '';
+
+// Lunes a domingo, en orden. Horas en formato 24 h.
+export const week = [
+	{ day: 'Lunes', open: 8, close: 18 },
+	{ day: 'Martes', open: 8, close: 18 },
+	{ day: 'Miércoles', open: 8, close: 18 },
+	{ day: 'Jueves', open: 8, close: 18 },
+	{ day: 'Viernes', open: 8, close: 18 },
+	{ day: 'Sábado', open: 8, close: 17 },
+	{ day: 'Domingo', open: 8, close: 17 },
+];
+
+export const hoursSummary = [
+	{ days: 'Lunes a viernes', time: '8:00 – 18:00' },
+	{ days: 'Sábados y domingos', time: '8:00 – 17:00' },
+];
